2467fe913764ddd5907c11d2628b0d3279922a77
khloudmousad <khloudmousad@users.noreply.github.com>
2021-10-26 14:13:49 +0000

    ✅ Ready to clone and code.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for pppp package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("pppp", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="pppp",
+    version=read("pppp", "VERSION"),
+    description="Awesome pppp created by khloudmousad",
+    url="https://github.com/khloudmousad/pppp/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="khloudmousad",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["pppp = pppp.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
